a6eab265c0ff80e52bcd2339df97890cfdc2847f
geopras <geopras84@gmail.com>
2016-10-24 20:48:06 +0200

    gulpfile erweitert

--- src/main/webapp/node/gulpfile.js
+++ src/main/webapp/node/gulpfile.js
@@ -12,15 +12,18 @@ gulp.task('jsTransform', function(){
     console.log('js wird neu generiert');
     // Auswahl der Ausgangsdateien
     return gulp.src([
+        '../dev/core/namespace.js',
+        '../dev/core/messageBroker.js',
+        '../dev/core/navigator.js',
         '../dev/controller/**/*',
         '../dev/view/javascript/**/*'
     ])
     //Verkettung der Dateien
-        .pipe(concat('ideaWatcher.js'))
+        .pipe(concat('ideaWatchers.js'))
         //schreiben der Dateien auf Festplatte
         .pipe(gulp.dest('../dist/private'))
         // Datei in Strom umbenennen
-        .pipe(rename('ideaWatcher.min.js'))
+        .pipe(rename('ideaWatchers.min.js'))
         // minifizieren
         .pipe(uglify())
         // auf Festplatte schreiben
@@ -30,7 +33,7 @@ gulp.task('jsTransform', function(){
 //region less -> css
 gulp.task('less', function () {
     console.log('less -> css');
-    return gulp.src(['../dev/view/less/concat/ideaWatcher.less'])
+    return gulp.src(['../dev/view/less/concat/ideaWatchers.less'])
         .pipe(less())
         .pipe(gulp.dest('../dist/private'));
 });
@@ -41,9 +44,29 @@ gulp.task('less', function () {
 // aktuellen Tasks ausgeführt werden sollen
 gulp.task('minicss', ['less'], function () {
     console.log('minifiziere css ...');
-    return gulp.src('../dist/private/ideaWatcher.css')
+    return gulp.src('../dist/private/ideaWatchers.css')
         .pipe(cssnano())
-        .pipe(rename('ideaWatcher.min.css'))
+        .pipe(rename('ideaWatchers.min.css'))
         .pipe(gulp.dest('../dist/public'));
 });
 //endregion
+
+gulp.task('buildAll', ['jsTransform', 'minicss'], function(){
+    console.log('Javascript und CSS minimiert und HTML neu zusammengebaut');
+    // Auswahl der Ausgangsdateien -> Reihenfolge wichtig!
+    return gulp.src([
+        '../dev/view/html/head.html',
+        '../dev/view/html/login-view.html',
+        '../dev/view/html/ideaList-view.html',
+        '../dev/view/html/ideaDetails-view.html',
+        '../dev/view/html/ideaCreation-view.html',
+        '../dev/view/html/profile-view.html',
+        '../dev/view/html/profileEdit-view.html',
+        '../dev/view/html/impress-view.html',
+        '../dev/view/html/foot.html'
+    ])
+    //Verkettung der Dateien
+        .pipe(concat('ideaWatchers.html'))
+        // auf Festplatte schreiben
+        .pipe(gulp.dest('../dist/public'));
+});
